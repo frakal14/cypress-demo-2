766e3ef235c3ad7e712e25ff435d972f86a7169e
[karol fraczek] <karol.fraczkowski@gmail.com>
2024-01-23 16:02:33 +0100

    Added login data fixture

--- cypress/e2e/01-signInTest.spec.js
+++ cypress/e2e/01-signInTest.spec.js
@@ -1,7 +1,14 @@
+const { onNavigation } = require("../support/page_objects/navigationPage")
+
+
 describe('Verify sign in page functionality ', () => {
+
+    beforeEach('Open homepage', () => {
+        cy.openHomePage()
+    })
     
     it('User is able to sign in with correct data', () => {
-        
+        onNavigation.clickOnSignIn
 
 
 
